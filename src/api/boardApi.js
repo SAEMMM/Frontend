f6330e8d68eab02ab60b681bb555a204032650b3
sbchoi5451 <sbchoi5451@gmail.com>
2023-05-10 13:54:42 +0900

    [feat] delete api url 변경

--- src/api/boardApi.js
+++ src/api/boardApi.js
@@ -42,7 +42,7 @@ export const deleteBoard = async ([id, accessToken, refreshToken]) => {
         }
     } 
     console.log(config)
-    return await boardApi.delete(`/api/board/${id}`, config)
+    return await boardApi.delete(`/api/boards/${id}`, config)
 }
 
 export default boardApi
