053ecf914c30949f902ad4993a03be3752e4bc3a
SAEMMM <saem030@naver.com>
2023-05-10 13:50:18 +0900

    [feat] board deleteApi 수정

--- src/api/boardApi.js
+++ src/api/boardApi.js
@@ -34,10 +34,6 @@ export const addPost = async ([formData, accessToken, refreshToken]) => {
     return await boardApi.post("/api/board", formData, config)
 }
 
-<<<<<<< HEAD
-export const deleteBoard = async ({ id }) => {
-    return await boardApi.delete(`/api/boards/${id}`, id)
-=======
 export const deleteBoard = async ([id, accessToken, refreshToken]) => {
     const config = {
         headers: {
@@ -47,7 +43,6 @@ export const deleteBoard = async ([id, accessToken, refreshToken]) => {
     } 
     console.log(config)
     return await boardApi.delete(`/api/board/${id}`, config)
->>>>>>> b7181a1d5ec15ea27ecd04c72957e3185faac898
 }
 
 export default boardApi
